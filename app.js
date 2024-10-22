360d83f42c7b4c41127bab8e954a04efeb518016
avleen19 <123055235+avleen19@users.noreply.github.com>
2024-10-22 15:13:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,25 +3,47 @@ const app=express();
 const PORT=8080;
 const path=require("path");
 const filepath=path.join(__dirname,"./views/index.ejs");
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
+// app.use(express.urlencoded({ extended: true })); 
+
+
 // app.get("/",(req,res)=>{
 //     let name="Sam";
 //     let place="Bengaluru"
 //     res.render(filepath,{name,destination:place});
 // })
-app.get("/welcome", (req, res) => {
-    let username = req.query.username || 'sam';  
-    let currentHour = new Date().getHours();
-    let greeting;
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+let tasks = []; 
 
-    if (currentHour < 12) {
-        greeting = "Good Morning";
-    } else {
-        greeting = "Good Evening";
-    }
-    res.render(filepath, { username, greeting });
+app.get("/welcome", (req, res) => {
+    let username = req.query.username || "sam";
+    let currentHour = new Date().getHours();
+    let greeting = currentHour < 12 ? "Good Morning" : "Good Evening";
+    res.render("index", { username, greeting });
+});
+app.get("/todo", (req, res) => {
+    // Render 'index2.ejs' (for to-do list)
+    res.render("index2", { tasks });
 });
 
+// Add a new task
+app.post("/addTask", (req, res) => {
+    let newTask = req.body.task;
+    if (newTask) {
+        tasks.push(newTask); // Add task to the array
+    }
+    res.redirect("/todo"); // Redirect to the to-do page after adding task
+});
+
+// Delete a task
+app.post("/deleteTask", (req, res) => {
+    let taskIndex = req.body.taskIndex;
+    if (taskIndex >= 0 && taskIndex < tasks.length) {
+        tasks.splice(taskIndex, 1); // Remove task from the array
+    }
+    res.redirect("/todo"); // Redirect to the to-do page after deleting task
+});
 app.listen(PORT,(err)=>{
     if(err){
         console.log(err);
